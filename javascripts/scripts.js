1213f177d2c9d343b7a60ec34dc1951a99927749
Nino Chekheriya <nino.chekher@gmail.com>
2026-06-19 22:43:37 +0300

    специалисты

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -189,52 +189,54 @@ window.addEventListener("load", () => {
 const lifeWhiteSector = document.querySelector(".lifeWhiteSector");
 const lifeWhiteButton = document.querySelector(".lifeWhiteButton");
 
-lifeWhiteButton.addEventListener("click", (event) => {
-  const rect = lifeWhiteButton.getBoundingClientRect();
+if (lifeWhiteSector && lifeWhiteButton) {
+  lifeWhiteButton.addEventListener("click", (event) => {
+    const rect = lifeWhiteButton.getBoundingClientRect();
 
-  const ripple = document.createElement("span");
-  ripple.classList.add("ripple");
+    const ripple = document.createElement("span");
+    ripple.classList.add("ripple");
 
-  ripple.style.left = `${event.clientX - rect.left}px`;
-  ripple.style.top = `${event.clientY - rect.top}px`;
+    ripple.style.left = `${event.clientX - rect.left}px`;
+    ripple.style.top = `${event.clientY - rect.top}px`;
 
-  lifeWhiteButton.appendChild(ripple);
-
-  setTimeout(() => {
-    ripple.remove();
-  }, 100);
-
-  lifeWhiteSector.classList.toggle("is-up");
-});
-
-lifeWhiteButton.addEventListener("mouseenter", (event) => {
-  const oldRipple = lifeWhiteButton.querySelector(".hover-ripple");
-
-  if (oldRipple) {
-    oldRipple.remove();
-  }
-
-  const rect = lifeWhiteButton.getBoundingClientRect();
-
-  const hoverRipple = document.createElement("span");
-  hoverRipple.classList.add("hover-ripple");
-
-  hoverRipple.style.left = `${event.clientX - rect.left}px`;
-  hoverRipple.style.top = `${event.clientY - rect.top}px`;
-
-  lifeWhiteButton.appendChild(hoverRipple);
-});
-
-lifeWhiteButton.addEventListener("mouseleave", () => {
-  const hoverRipple = lifeWhiteButton.querySelector(".hover-ripple");
-
-  if (hoverRipple) {
-    hoverRipple.classList.add("is-leaving");
+    lifeWhiteButton.appendChild(ripple);
 
     setTimeout(() => {
-      hoverRipple.remove();
-    }, 450);
-  }
-});
+      ripple.remove();
+    }, 100);
+
+    lifeWhiteSector.classList.toggle("is-up");
+  });
+
+  lifeWhiteButton.addEventListener("mouseenter", (event) => {
+    const oldRipple = lifeWhiteButton.querySelector(".hover-ripple");
+
+    if (oldRipple) {
+      oldRipple.remove();
+    }
+
+    const rect = lifeWhiteButton.getBoundingClientRect();
+
+    const hoverRipple = document.createElement("span");
+    hoverRipple.classList.add("hover-ripple");
+
+    hoverRipple.style.left = `${event.clientX - rect.left}px`;
+    hoverRipple.style.top = `${event.clientY - rect.top}px`;
+
+    lifeWhiteButton.appendChild(hoverRipple);
+  });
+
+  lifeWhiteButton.addEventListener("mouseleave", () => {
+    const hoverRipple = lifeWhiteButton.querySelector(".hover-ripple");
+
+    if (hoverRipple) {
+      hoverRipple.classList.add("is-leaving");
+
+      setTimeout(() => {
+        hoverRipple.remove();
+      }, 450);
+    }
+  });
+}
 
 // СТРАНИЦА О НАС
